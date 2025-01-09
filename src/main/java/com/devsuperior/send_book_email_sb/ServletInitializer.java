121a5f8ef9b22cbe74ea7789acd33ee54bbdc18a
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-09 12:19:40 -0300

    criando o projeto

---
+++ src/main/java/com/devsuperior/send_book_email_sb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.devsuperior.send_book_email_sb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SendBookEmailSbApplication.class);
+	}
+
+}
