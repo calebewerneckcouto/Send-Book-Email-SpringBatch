216df369aaa3180491cce625fb9b7227db0bf803
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2025-01-09 22:15:21 -0300

    Update SendBookLoanNotificationScheduleJob.java

--- src/main/java/com/devsuperior/send_book_email_sb/job/SendBookLoanNotificationScheduleJob.java
+++ src/main/java/com/devsuperior/send_book_email_sb/job/SendBookLoanNotificationScheduleJob.java
@@ -1,5 +1,6 @@
 package com.devsuperior.send_book_email_sb.job;
 
+// Importa as classes necessárias para integrar o Quartz com o Spring Batch
 import org.quartz.JobExecutionContext;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -10,26 +11,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+// Define esta classe como uma configuração Spring
 @Configuration
 public class SendBookLoanNotificationScheduleJob extends QuartzJobBean {
-	
-	@Autowired
-	private Job job;
-	
-	@Autowired
-	private JobExplorer jobExplorer;
-	
-	@Autowired
-	private JobLauncher jobLaucher;
+    
+    // Injeta o bean do Spring Batch que representa o Job a ser executado
+    @Autowired
+    private Job job;
+    
+    // Injeta o JobExplorer, que é usado para explorar e consultar os jobs no Spring Batch
+    @Autowired
+    private JobExplorer jobExplorer;
+    
+    // Injeta o JobLauncher, responsável por iniciar a execução dos jobs no Spring Batch
+    @Autowired
+    private JobLauncher jobLaucher;
 
-	@Override
-	protected void executeInternal(JobExecutionContext context) {
-		JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer).getNextJobParameters(this.job).toJobParameters();
-		try {
-			this.jobLaucher.run(this.job, jobParameters);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+    // Método que será executado pelo Quartz quando o Job for acionado
+    @Override
+    protected void executeInternal(JobExecutionContext context) {
+        // Cria parâmetros para o Job usando o JobExplorer
+        JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer)
+                .getNextJobParameters(this.job) // Obtém os próximos parâmetros válidos para o Job
+                .toJobParameters(); // Converte para o formato de JobParameters
+        
+        try {
+            // Executa o Job com os parâmetros criados
+            this.jobLaucher.run(this.job, jobParameters);
+        } catch (Exception e) {
+            // Trata possíveis exceções que ocorram durante a execução do Job
+            e.printStackTrace();
+        }
+    }
 }
