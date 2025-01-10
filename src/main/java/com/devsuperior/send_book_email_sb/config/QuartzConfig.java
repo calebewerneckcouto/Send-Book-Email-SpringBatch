a93bb11a52218e66e1850cb924d209d3f04a6341
CALEBE WERNECK COUTO <59000781+calebewerneckcouto@users.noreply.github.com>
2025-01-09 22:14:40 -0300

    Update QuartzConfig.java

--- src/main/java/com/devsuperior/send_book_email_sb/config/QuartzConfig.java
+++ src/main/java/com/devsuperior/send_book_email_sb/config/QuartzConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.send_book_email_sb.config;
 
+// Importa as classes necessárias para configurar o Quartz Scheduler
 import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
@@ -8,24 +9,33 @@ import org.quartz.TriggerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Importa a classe do job que será agendado
 import com.devsuperior.send_book_email_sb.job.SendBookLoanNotificationScheduleJob;
 
+// Define que esta classe é uma configuração Spring
 @Configuration
 public class QuartzConfig {
 
-	@Bean
-	public JobDetail quartzJobDetail() {
-		return JobBuilder.newJob(SendBookLoanNotificationScheduleJob.class).storeDurably().build();
-	}
+    // Define um bean para o JobDetail, que representa a tarefa (Job) que será executada
+    @Bean
+    public JobDetail quartzJobDetail() {
+        return JobBuilder.newJob(SendBookLoanNotificationScheduleJob.class) // Associa o Job à classe que contém a lógica de execução
+                .storeDurably() // Indica que este Job será armazenado permanentemente, mesmo que não tenha gatilhos associados
+                .build(); // Constrói o JobDetail
+    }
 
-	@Bean
-	public Trigger jobTrigger() {
-		String exp = "0 38 21 * * ?";  //informar hora que vc quer que seja enviado...
-		return TriggerBuilder
-				.newTrigger()
-				.forJob(quartzJobDetail())
-				.startNow()
-				.withSchedule(CronScheduleBuilder.cronSchedule(exp))
-				.build();
-	}
+    // Define um bean para o Trigger, que especifica quando o Job será executado
+    @Bean
+    public Trigger jobTrigger() {
+        String exp = "0 38 21 * * ?"; 
+        // Expressão Cron que define a frequência de execução do Job
+        // Neste exemplo, o Job será executado às 21:38 todos os dias
+
+        return TriggerBuilder
+                .newTrigger() // Cria um novo Trigger
+                .forJob(quartzJobDetail()) // Associa o Trigger ao Job configurado
+                .startNow() // Inicia o Trigger imediatamente após a configuração
+                .withSchedule(CronScheduleBuilder.cronSchedule(exp)) // Define a frequência usando a expressão Cron
+                .build(); // Constrói o Trigger
+    }
 }
